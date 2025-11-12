a55cba8f244c350338573faec47796afe9f5fffd
Murad Ahmadov <akmadoff.10@gmail.com>
2025-11-13 00:25:25 +0400

    js lessons

---
+++ Stage2/Topic1/intro.js
@@ -0,0 +1,28 @@
+// console.log("Hello, JavaScript!");
+
+
+// Primitive & InPrimitive
+
+//NUM
+var age = 25; // 1.NUM / INT=25 FLOAT=12.5
+var allSalary = 10025; // 1.NUM / INT=25 FLOAT=12.5
+var workingMonth = 14
+
+//STRING
+var fullname = "Murad Ahmadov"
+var fullname2 = 'Murad Ahmadov'
+var fullname3 = `Murad Ahmadov`
+
+//BOOLEAN
+var  isNew = false
+var  isWorking = true
+
+//NULL
+var productImg = "https://kontak.az/mebeller/divan.png"
+var productImg = null
+
+//UNDEFINED
+var password = undefined
+
+
+
---
+++ Stage2/Topic1/operator.js
@@ -0,0 +1,29 @@
+//Arithmetic Operators Example
+
+// var a = 20 
+// var b = 30
+
+// var c = a + b
+
+// console.log(c);
+
+// var appointmentPrice = 200;
+// var employeePersentage = 40;
+// var employeeProfit = (appointmentPrice * employeePersentage) /100;
+// var branchProfit = appointmentPrice - employeeProfit;
+
+
+
+// console.log("Isci massi",employeeProfit);
+// console.log("Sirket qazanci",branchProfit)
+
+// Comparision Operators
+var x = 5
+var y = "5"
+
+//var result = x > y // true
+//var result = x == y // false
+//var result = x !== y // true 
+
+
+console.log(result)

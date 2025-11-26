6efe32e3d06af23eabb82e1ddaad7b10e0bd8dcf
Murad Ahmadov <akmadoff.10@gmail.com>
2025-11-26 18:06:30 +0400

    Add initial HTML structure and JavaScript methods for array manipulation

---
+++ Stage2/Topic2/index.js
@@ -0,0 +1,64 @@
+// Methods
+
+let numbers = [10, 20, 30, 40, 50,[100,200,300]];
+
+//PUSH
+
+// numbers.push(60); // adds 60 at the end
+
+// UNSHIFT
+// numbers.unshift(15); // removes first element (10)
+
+// POP
+// numbers.pop(); // removes last element (50)
+
+// // SHIFT
+// numbers.shift(); // removes first element (10) 
+
+// numbers.splice(1,2); // at index 2, removes 0 elements
+
+// let lastindex = numbers.length - 1; // returns length of array
+// numbers.splice(lastindex - 2 , 3); // returns last element of array
+
+// numbers.splice(2,null,"Baki","Qax")
+
+
+// SLICE
+// let result = numbers.slice(0,4); // from index 1 to index 4 (4 not included)
+
+// console.log("result:",result);
+
+
+// let fruits = ["Banana", "Orange", "Apple", "Mango"];
+
+// let moreList = numbers.concat(fruits); // combines two arrays
+
+// console.log("moreList:",moreList);
+
+
+// CONCAT
+
+// let numbers2 = numbers.concat(); // combines two arrays 
+
+// console.log("numbers2:",numbers2);
+
+
+// let result = numbers.includes(10); // checks if 10 is in the array
+// let result = numbers.indexOf(30); // returns index of 30
+// let result = last.Indexof(30); // returns last index of 30
+// let result = numbers.join(" "); // joins array elements into a string with "-" separator
+// let result = numbers.flat(); // flattens nested arrays
+// console.log("result:",result);
+
+
+// LAST INDEX OF
+
+// let result = numbers[LastIndex]
+// console.log("lastindex:",lastindex);
+// console.log("result:",result);
+
+
+
+
+
+console.log("numbers:",numbers);
---
+++ Stage2/Topic2/intro.js
@@ -0,0 +1,33 @@
+
+    //INTRO
+
+    // let numbers = [10, 20, 30, 40, 50];
+    // let students = ["Alice", "Bob", "Charlie", "David"];
+
+    // let result = numbers[4]
+    // let result2 = students[2]
+
+    // console.log(result);
+    // console.log(result2);
+
+
+    // Heap and Stack memorise
+
+
+    // STACK
+
+    // let a = 10;
+    // let b = a;
+    // a = 20;
+
+    // console.log("a:",a); // 20
+    // console.log("b:",b); // 10
+
+     // HEAP
+     let numbers = [10, 20, 30, 40, 50];
+     let numbers2 = numbers;
+
+    //  numbers[0] = 100;
+
+     console.log("numbers2:", numbers2);
+     console.log("numbers:", numbers);

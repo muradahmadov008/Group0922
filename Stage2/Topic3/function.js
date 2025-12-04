536b67c53743f25427c2d0dc66f44cbbc8819b95
Murad Ahmadov <akmadoff.10@gmail.com>
2025-12-05 01:55:53 +0400

    Add initial JavaScript functions and HTML structure for Topic 3

---
+++ Stage2/Topic3/function.js
@@ -0,0 +1,132 @@
+// // Function
+
+// function A(){
+//     let age = 25;
+//     console.log("function A",age)
+
+//     // your logic here
+// }
+
+// A();
+
+
+// function sumCalculator(num1, num2){
+//     let sum = num1 + num2;
+//     console.log("Sum is:", sum);
+
+// }
+// function multiblerCalculator(num1, num2){
+//     let sum = num1 * num2;
+//     console.log("Multibler is:", sum);
+
+// }
+
+// sumCalculator(10, 20);
+// sumCalculator(40, 50);
+// sumCalculator(30, 60);
+
+// multiblerCalculator(10, 20);
+// multiblerCalculator(2, 1);
+// multiblerCalculator(100, 20);
+
+
+
+// function sumCalculator(num1, num2,num3) {
+//   let sum = num1 + num2 + num3;
+  
+//   return sum;
+// }
+
+// let result = sumCalculator(5,10,15);
+// console.log("Result is:",result)
+
+
+// function greetUser(name,balance){
+//     let result =`Hello,${name}! Welcome to the platform. Your balance is $${balance}.`;
+//     return result;
+// }
+
+// let message = greetUser("Mike",100);
+// let message2 = greetUser("Fazil",400);
+// let message3 = greetUser("Joe",200);
+
+
+// console.log(message);
+// console.log(message2);
+// console.log(message3);
+
+
+
+
+// let fruits = ["apple", "orange","lemon", "pie",];
+// // let numbers = [10, 20, 30, 40,50];
+// let students = ["Joe", "Silva", "Fateh", "Jale"];
+
+
+// function printArrayEL(array){
+//     console.log("===========");
+
+//     for (let i =0; i < array.length; i++){
+//         console.log("Array", array[i]);
+
+//     }
+// }
+
+// printArrayEL(fruits)
+// // printArrayEL(numbers)
+// printArrayEL(students)
+
+
+// let fruits = ["apple", "orange", "lemon", "pie"];
+// let students = ["Joe", "Silva", "Fateh", "Jale"];
+
+// function printArrayEL(strArray){
+//     let resultArray = [];
+
+//     for (let i = 0; i < strArray.length; i++){
+
+//         let el = strArray[i]
+
+//         if(el.toLowerCase().includes("l")){
+//             resultArray.push(el);
+//         }
+//     }
+//     return resultArray;
+// }
+
+// let result1 = printArrayEL(fruits)
+// let result2 = printArrayEL(students)
+
+
+// console.log("Result 1:", result1)
+// console.log("Result 2:", result2)
+
+
+
+
+// let fruits = ["apple", "orange", "lemon", "pie"];
+// let students = ["Joe", "Silva", "Fateh", "Jale"];
+
+// function printArrayEL(strArray, char) {
+//   let resultArray = [];
+
+//   for (let i = 0; i < strArray.length; i++) {
+//     let el = strArray[i];
+
+//     if (el.toLowerCase().includes(charLower)) {
+//       resultArray.push(el);
+//     }
+//   }
+//   return resultArray;
+// }
+
+// let result1 = printArrayEL(fruits,"d");
+// let result2 = printArrayEL(students,"d");
+
+// console.log("Result 1:", result1);
+// console.log("Result 2:", result2);
+
+
+
+
+
---
+++ Stage2/Topic3/index.js
@@ -0,0 +1,101 @@
+
+// // STACK
+
+// let a = 5
+
+// let b = a
+
+// a = 25
+
+// console.log("a:", a) // 25
+// console.log("b:", b) // 5
+
+// // HEAP
+
+// let fruits = ['Apple', 'Banana', 'Cherry']
+
+// let moreFruits = fruits
+
+// fruits.push('Orange')
+
+// console.log("fruits:", fruits)         // ['Apple', 'Banana', 'Cherry', 'Date']
+// console.log("moreFruits:", moreFruits) // ['Apple', 'Banana', 'Cherry', 'Date'].
+
+
+// LOOPS
+
+// let array = [];
+
+// for (let count = 0; count < 100; count ++) {
+
+//     if( count% 5 === 0 ){
+//     array .push(count);
+    
+//   }
+// }
+
+// console.log("array", array);
+
+
+
+
+// for (let i = 0; i < fruits.length; i++) {
+
+//     let el= fruits[i];
+
+    
+
+//     // if (el == 'Cherry') {
+//     //     break;
+//     // }
+
+//     if (el == 'Cherry' || el == 'Apple') {
+//         continue;
+//     }
+
+//     console.log("After Continue:", el);
+// }
+
+// let fruits = ['Apple', 'Banana', 'Cherry', 'Date', 'Elderberry'];
+// let newFruits = [];
+
+// for (let i = 0; i < fruits.length; i++){
+
+//         let el = fruits[i];
+
+//         if(el.toLowerCase()[0]=='a'){
+//         console.log("You  have", el);
+//         newFruits.push(el);
+//         }
+//         console.log("newFruits", newFruits);
+    
+//     // your logic here
+// }
+
+// let students = ["John", "Mike", "Jim", "Frank", "Leo"];
+
+// let i = 0; 
+
+// for(let element of students){
+//     console.log("Student Name:" , element);
+//  }
+
+let car = ["Mercedes", "Bmw", "Audi", "Toyota"];
+
+let i = 0;
+
+for(let element of car){
+    console.log("Car name:", element);
+}
+
+// while (i < students.length) {
+
+//     let student = students[i];
+
+//     console.log("Student Name:", student);
+
+//     // your logic here
+
+//      i++;
+// }
+
